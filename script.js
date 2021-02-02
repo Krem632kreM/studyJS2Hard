7739d97a76ff927ec8b83c997fea012bd6bb9aed
Tatyana <krempinatatyana@gmail.com>
2021-02-03 00:15:04 +0400

    3 урок усложнённое задание

---
+++ script.js
@@ -0,0 +1,37 @@
+let lang = prompt("Введите значение lang");
+
+ if (lang === 'ru') {
+    console.log ("понедельник вторник среда четверг пятница суббота воскресение");
+    } else if (lang === 'en') {
+    console.log ("Monday Tuesday Wednesday Thursday Friday Saturday Sunday");
+    } else {
+    console.log("lang может принимать 2 значения: \'ru\' \'en\'");
+    }
+
+    switch(lang)
+    {
+        case 'ru':
+            console.log ("понедельник вторник среда четверг пятница суббота воскресение");
+            break;
+        case 'en':
+            console.log ("Monday Tuesday Wednesday Thursday Friday Saturday Sunday");
+            break;
+        default:
+            console.log("lang может принимать 2 значения: \'ru\' \'en\'");
+    }
+
+      
+    var arr = {
+        'ru':['понедельник', 'вторник', 'среда','четверг', 'пятница', 'суббота', 'воскресение'],
+        'en':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    };
+    
+    console.log(arr[lang].join(" "));
+      
+let namePerson = prompt("Введите имя");
+let result;
+
+result = (namePerson === 'Артем') ? 'директор' : 'студент';
+result = (namePerson === 'Максим') ? 'преподаватель' : 'студент';
+ 
+console.log(result);
